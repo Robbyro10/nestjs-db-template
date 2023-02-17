55fc9b161647c15c718661da8a85fc13ea2cd1eb
Robbyro10 <63318506+Robbyro10@users.noreply.github.com>
2023-02-17 13:35:54 -0400

    first commit

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,78 @@
+import { Transform } from 'class-transformer';
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BeforeUpdate, OneToMany } from 'typeorm';
+import { ProductImage } from './product-image.entity';
+
+@Entity({ name: 'products' })
+export class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', {
+        unique: true
+    })
+    title: string;
+
+    @Column('float', {
+        default: 0
+    })
+    price: number;
+
+    @Column('text', {
+        nullable: true
+    })
+    description: string;
+
+    @Column('text', {
+        unique: true
+    })
+    slug: string;
+
+    @Column('int', {
+        default: 0
+    })
+    stock: number;
+
+    @Column('text', {
+        array: true
+    })
+    sizes: string[];
+
+    @Column('text')
+    gender: string;
+
+    @Column('text', {
+        array: true,
+        default: []
+    })
+    tags: string[];
+
+    @OneToMany(
+        () => ProductImage,
+        (productImage) => productImage.product,
+        { cascade: true, eager: true }
+    )
+    @Transform(({ value }) => {
+        return value.map((image: ProductImage) => image.url);
+    })
+    images?: ProductImage[];
+
+    @BeforeInsert()
+    checkSlugInsert() {
+        if (!this.slug) this.slug = this.title;
+
+        this.slug = this.slug.toLowerCase()
+            .replaceAll(" ", "_")
+            .replaceAll("'", "")
+    }
+
+    @BeforeUpdate()
+    checkSlugUpdate() {
+        if (!this.slug) this.slug = this.title;
+
+        this.slug = this.slug.toLowerCase()
+            .replaceAll(" ", "_")
+            .replaceAll("'", "")
+    }
+
+}
